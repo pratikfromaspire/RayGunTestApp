ca98bcb3436f2b8368a315cf81e177eea5f3804b
Pratik Patel <pratik.patel@aspiresoftware.in>
2015-02-07 10:50:38 +0530

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rayGunTestAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rayGunTestAppApp
+ */
+angular.module('rayGunTestAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rayGunTestAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rayGunTestAppApp
+ */
+angular.module('rayGunTestAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
